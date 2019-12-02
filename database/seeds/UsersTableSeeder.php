e898944c656034faf32b75b6180d289fcfecf32a
Le Dinh Huan <huan.ld@neo-lab.vn>
2019-12-02 16:30:14 +0700

    add user

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -12,11 +12,27 @@ class UsersTableSeeder extends Seeder
     public function run()
     {
         $data = [
-            'name' => 'Admin',
-            'email' => 'huan.ld@neo-lab.vn',
-            'password' => '$2y$10$/Ti53ez.QOFILU4wsJ5QjePH8TLHORdQJx6KyMh7lQHSpYRiN2aG.',
-            'is_admin' => 1,
-            'image' => 'https://img.icons8.com/clouds/100/000000/guest-male.png'
+            [
+                'name' => 'Admin Huan',
+                'email' => 'huan.ld@neo-lab.vn',
+                'password' => '$2y$10$/Ti53ez.QOFILU4wsJ5QjePH8TLHORdQJx6KyMh7lQHSpYRiN2aG.',
+                'is_admin' => 1,
+                'image' => 'https://img.icons8.com/clouds/100/000000/guest-male.png'
+            ],
+            [
+                'name' => 'Khuong',
+                'email' => 'khuong@gmail.com',
+                'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',
+                'is_admin' => 1,
+                'image' => 'https://img.icons8.com/clouds/100/000000/guest-male.png'
+            ],
+            [
+                'name' => 'Thuan',
+                'email' => 'thuan@gmail.com',
+                'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',
+                'is_admin' => 1,
+                'image' => 'https://img.icons8.com/clouds/100/000000/guest-male.png'
+            ]
         ];
         \DB::table('users')->insert($data);
     }
